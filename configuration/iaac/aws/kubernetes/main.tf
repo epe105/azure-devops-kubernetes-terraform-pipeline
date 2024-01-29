33685d2b14f5cb408a5352f934df830ba9a99759
epe105 <19913916+epe105@users.noreply.github.com>
2024-01-29 16:43:58 -0500

    copied over project
    update to main.tf
    
    update to final-main.tf

---
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -0,0 +1,85 @@
+# aws --version
+# aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
+# Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
+# terraform-backend-state-manny703
+# 
+
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-backend-state-manny703" # Will be overridden from build
+    key    = "path/to/my/key" # Will be overridden from build
+    region = "us-east-1"
+  }
+}
+
+#resource "aws_default_vpc" "default" {
+resource "mann-vpc" "default" {
+
+
+}
+
+data "aws_subnet_ids" "subnets" {
+  #vpc_id = aws_default_vpc.default.id
+  vpc_id = vpc-0d7b7290df82bc3d5
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  version                = "~> 2.12"
+}
+
+module "in28minutes-cluster" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "in28minutes-cluster"
+  cluster_version = "1.14"
+  subnets         = ["subnet-013eda54a78e0115e", "subnet-057cd2aee058e4e00"] #CHANGE
+  #subnets = data.aws_subnet_ids.subnets.ids
+  #vpc_id          = aws_default_vpc.default.id
+
+  vpc_id         = "vpc-0d7b7290df82bc3d5"
+
+  node_groups = [
+    {
+      instance_type = "t2.micro"
+      max_capacity  = 5
+      desired_capacity = 3
+      min_capacity  = 3
+    }
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.in28minutes-cluster.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.in28minutes-cluster.cluster_id
+}
+
+
+# We will use ServiceAccount to connect to K8S Cluster in CI/CD mode
+# ServiceAccount needs permissions to create deployments 
+# and services in default namespace
+resource "kubernetes_cluster_role_binding" "example" {
+  metadata {
+    name = "fabric8-rbac"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "default"
+    namespace = "default"
+  }
+}
+
+# Needed to set the default region
+provider "aws" {
+  region  = "us-east-1"
+}
